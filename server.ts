32354b8d21515d11dcbafdb2bd7030bf0c9a0571
Ahmed Mubarak <ahmedmubarak.hsin@gmail.com>
2024-03-10 07:31:56 +0200

    init commit

---
+++ server.ts
@@ -0,0 +1,12 @@
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT || 8000;
+
+
+app.listen(port, () => {
+  console.log(`Server is Running at port: ${port}`);
+});
