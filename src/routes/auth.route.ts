f4aaebd4c8cda5aa3dd644720c63c6bb1ed76281
Ahmed Mubarak <ahmedmubarak.hsin@gmail.com>
2024-04-02 10:15:09 +0200

    update login status codes - add logout

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -6,17 +6,27 @@ const router = Router()
 router.post('/login', (req, res, next) => {
     const username = req.body.username;
     const password = req.body.password;
-    next()
-},
-    passport.authenticate('local', {
-        successRedirect: 'api/auth/success',
-        failureRedirect: 'api/auth/fail'
-    })
-);
-router.get('/success', (req, res) => {
-    res.status(200).json({ message: 'Login success' });
+    passport.authenticate('local', (err: any, user: any, info: any) => {
+        if (err) {
+          return next(err);
+        }
+    
+        if (!user) {
+          return res.status(401).json({ message: 'Invalid username or password' });
+        }
+    
+          // Send success response with relevant code data in JSON format
+          return res.status(200).json({
+            message: 'Login successful',
+          });
+        })(req, res, next);
 });
-router.get ('/fail', (req, res) => {
-    res.status(401).json({ message: 'Login fail' });
+router.get('/logout', (req, res, next) => {
+    req.logout((err) => {
+        if (err) {
+            next(err)
+        }
+        res.status(200).json({ message: 'Logout successful' });
+    });
 });
 export default router;
